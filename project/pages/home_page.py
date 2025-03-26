62c3af240d6522fdf8774d128a17717740380e31
jdiaz@clinicasanfelipe.com <a.diazcabanillas97@gmail.com>
2025-03-26 16:14:13 -0500

    sdsa

--- project/pages/home_page.py
+++ project/pages/home_page.py
@@ -1,6 +1,4 @@
 class HomePage:
     def __init__(self, driver):
         self.driver = driver
-        # Aquí puedes agregar los locators de la página principal
         
-    # Aquí puedes agregar métodos para interactuar con la página principal

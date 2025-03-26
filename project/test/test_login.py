7faf3608638f718f89c3bf1eadd87273c1996f29
jdiaz@clinicasanfelipe.com <a.diazcabanillas97@gmail.com>
2025-03-26 16:21:30 -0500

    qwq wqe

--- project/test/test_login.py
+++ project/test/test_login.py
@@ -2,6 +2,7 @@ import sys
 import os
 import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
@@ -14,7 +15,12 @@ def test_login():
         login_page = LoginPage(driver)
         (login_page.open()
                   .enter_credentials("74847349", "Tonydc1502%")
-                  .submit_login())
+                  .submit_login()
+                  )
+        time.sleep(20)
+
+        Citas=driver.find_element(By.id,"img_INI_HistorialCitas")
+        Citas.click()
         time.sleep(100000)
     finally:
         driver.quit()
